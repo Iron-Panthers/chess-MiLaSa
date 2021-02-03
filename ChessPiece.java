17e5d8c1332d6abf61b1610e7d77a7c60e23c762
Landon Stobaugh <landon.stobaugh@gmail.com>
2021-02-02 18:33:10 -0800

    General chess piece

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,7 +1,11 @@
 public class ChessPiece
 {
-  public ChessPiece()
+  //Public variable 
+  public static int pos[2];
+  public static String color;
+  public ChessPiece(int _pos[2], String _color)
   {
-
+    pos = _pos;
+    color = _color;
   }
 }
