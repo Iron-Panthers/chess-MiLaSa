1d020e667aa16687e79199ca4d968fd7551dcdc2
Landon Stobaugh <landon.stobaugh@gmail.com>
2021-02-09 18:15:14 -0800

    Main addition

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ public class Main
   {
     King new_king = new King(new int[]{1, 1}, "white");
     Pawn new_pawn = new Pawn(new int[]{1, 1}, "white");
+    Boolean test = new_pawn.valid_move(new int[]{2,2}, new String[]{"black"});
+    System.out.println(test);
   }
 }
