14432333ef8bb7f3e981779bdbfee222d0d4b1e0
Landon Stobaugh <landon.stobaugh@gmail.com>
2021-02-09 18:07:43 -0800

    Pawn rework

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main
   public static void main(String args[])
   {
     King new_king = new King(new int[]{1, 1}, "white");
+    Pawn new_pawn = new Pawn(new int[]{1, 1}, "white");
   }
 }
--- Pawn.java
+++ Pawn.java
@@ -11,13 +11,17 @@ public class Pawn extends ChessPiece
     if (color == "white")
     {
       //Movement
-      if ((pos[1] == 1 && new_pos[1] == 3 && new_tile_color[0] == "empty" && new_tile_color[1] == "empty" && new_pos[0] == pos[0])||(new_pos[1]-pos[1] == 1 && new_tile_color[0] == "empty" && new_pos[0] == pos[0])) return 1;
+      if ((pos[1] == 1 && new_pos[1] == 3 && new_tile_color[0] == "empty" && new_tile_color[1] == "empty" && new_pos[0] == pos[0])||(new_pos[1]-pos[1] == 1 && new_tile_color[0] == "empty" && new_pos[0] == pos[0])) return true;
       //Attack
-      if (Math.abs(new_pos[0]-pos[0])==1 && new_pos[1]-pos[1] == 1 && new_tile_color[0] == "black")
+      if (Math.abs(new_pos[0]-pos[0])==1 && new_pos[1]-pos[1] == 1 && new_tile_color[0] == "black") return true;
     }
     else 
     {
-      
+      //Movement
+      if ((pos[1] == 6 && new_pos[1] == 4 && new_tile_color[0] == "empty" && new_tile_color[1] == "empty" && new_pos[0] == pos[0])||(pos[1]-new_pos[1] == 1 && new_tile_color[0] == "empty" && new_pos[0] == pos[0])) return true;
+      //Attack
+      if (Math.abs(new_pos[0]-pos[0])==1 && pos[1]-new_pos[1] == 1 && new_tile_color[0] == "white") return true;
     }
+    return false;
   }
 }
