9c403df5ccc274b5a5d83f1ca50f16f2eca45283
sarah <23mins22@stu.smuhsd.org>
2021-02-18 18:30:13 -0800

    bishop
    + formatting in the other classes

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-public class Main
-{
-  public static void main(String args[])
-  {
+public class Main {
+  public static void main(String args[]) {
     King new_king = new King(new int[]{1, 1}, "white");
     Pawn new_pawn = new Pawn(new int[]{1, 1}, "white");
     Boolean test = new_pawn.valid_move(new int[]{2,2}, new String[]{"black"});
