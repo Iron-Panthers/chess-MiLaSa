8dcc7234e48f79ef10e3f872e8c96b5e4f9ca53b
Landon Stobaugh <landon.stobaugh@gmail.com>
2021-03-02 18:16:13 -0800

    some more fixes but still buggy

--- Bishop.java
+++ Bishop.java
@@ -38,10 +38,10 @@ public class Bishop extends ChessPiece
     }
 
     int i = 1;
-    while ((pos[0]+x_change*i>=0 && pos[0]+x_change*i < 8) && (pos[1]+x_change*i>=0 && pos[1]+x_change*i < 8)) 
+    while ((pos[0]+x_change*i>=0 && pos[0]+x_change*i < 8) && (pos[1]+y_change*i>=0 && pos[1]+y_change*i < 8)) 
     {
       System.out.println("X-Pos: "+String.valueOf(pos[0]+x_change*i));
-      return_string_vector.add(board_[pos[0]+x_change*i][pos[1]+y_change*i].color);
+      return_string_vector.add(board_[pos[1]+y_change*i][pos[0]+x_change*i].color);
       i++;
     }
   
@@ -52,6 +52,9 @@ public class Bishop extends ChessPiece
 
   public Boolean valid_move(int[] new_pos, ChessPiece[][] board_) {
 
+    if (new_pos[0] == pos[0] || new_pos[1] == pos[1]) return false;
+    System.out.println(String.valueOf(Math.abs((double)(new_pos[1]-pos[1])/(double)(new_pos[0]-pos[0]))));
+    if (Math.abs((double)(new_pos[1]-pos[1])/(double)(new_pos[0]-pos[0])) != 1.0) return false;
     String[] new_tile_color = get_tiles(board_,new_pos);
     System.out.println(Arrays.toString(new_tile_color));
       return false;
