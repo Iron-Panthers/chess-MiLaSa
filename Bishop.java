4a075c6c133a92b3ec50d192ed15c5e04a4bc368
Landon Stobaugh <landon.stobaugh@gmail.com>
2021-03-02 18:06:56 -0800

    Piece upgrades

--- Bishop.java
+++ Bishop.java
@@ -1,14 +1,59 @@
+import java.util.*;
+
 public class Bishop extends ChessPiece
 {
   public Bishop(int[] _pos, String _color) {
     super(_pos, _color);
   }
+  
+  public String[] get_tiles(ChessPiece[][] board_, int[] new_pos)
+  {
+    List<String> return_string_vector = new ArrayList<String>();
 
-  public Boolean valid_move(int[] new_pos, String new_tile_color) {
-    if ((Math.abs(pos[0]-new_pos[0])<=15) && (new_tile_color != color)) {
-      return true;
-    } else {
-      return false;
+    int x_change = 0;
+    int y_change = 0;
+    if (new_pos[0] > pos[0] && new_pos[1] > pos[1])
+    {
+      //Top right corner
+      x_change = 1;
+      y_change = 1;
     }
+    else if (new_pos[0] > pos[0] && new_pos[1] < pos[1])
+    {
+      //Bottom right corner
+      x_change = 1;
+      y_change = -1;
+    }
+    else if (new_pos[0] < pos[0] && new_pos[1] > pos[1])
+    {
+      //Top Left corner
+      x_change = -1;
+      y_change = 1;
+    }
+    else
+    {
+      //Bottom left corner
+      x_change = -1;
+      y_change = -1;
+    }
+
+    int i = 1;
+    while ((pos[0]+x_change*i>=0 && pos[0]+x_change*i < 8) && (pos[1]+x_change*i>=0 && pos[1]+x_change*i < 8)) 
+    {
+      System.out.println("X-Pos: "+String.valueOf(pos[0]+x_change*i));
+      return_string_vector.add(board_[pos[0]+x_change*i][pos[1]+y_change*i].color);
+      i++;
+    }
+  
+    String[] return_string = new String[return_string_vector.size()];
+    return_string_vector.toArray(return_string);
+    return return_string;
+  }
+
+  public Boolean valid_move(int[] new_pos, ChessPiece[][] board_) {
+
+    String[] new_tile_color = get_tiles(board_,new_pos);
+    System.out.println(Arrays.toString(new_tile_color));
+      return false;
   }
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+
+import java.util.*;
+
 public class Main
 {
   public static void main(String args[])
@@ -5,9 +8,13 @@ public class Main
     
     Board chess_board = new Board();
     chess_board.init_board();
+    Bishop new_bishop = new Bishop(new int[]{1, 1}, "white");
     King new_king = new King(new int[]{1, 1}, "white");
     Pawn new_pawn = new Pawn(new int[]{1, 1}, "white");
-    Boolean test = new_pawn.valid_move(new int[]{2,2}, new String[]{"black"});
+
+    System.out.println(Arrays.toString(chess_board.board_array));
+
+    Boolean test = new_bishop.valid_move(new int[]{2,2}, chess_board.board_array);
     System.out.println(test);
   }
 }
