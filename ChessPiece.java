92b115d64f66c59280091e0dc8f124e8dc8c755e
LandonStobaugh <71854345+LandonStobaugh@users.noreply.github.com>
2021-03-09 18:35:17 -0800

    Chess piece comment

--- ChessPiece.java
+++ ChessPiece.java
@@ -3,6 +3,8 @@ public class ChessPiece
   // Public variables that all chess pieces will inherit
   public int[] pos;
   public String color;
+  
+  // Constructor to assign parameters to different values
   public ChessPiece(int[] _pos, String _color)
   {
     pos = _pos;
